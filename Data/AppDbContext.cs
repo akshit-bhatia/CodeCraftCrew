5f26ca97058ce0b322c0a8c6f3ae8b6c44bfd845
Akshit Bhatia <akshitbhatiaprofile@gmail.com>
2023-10-05 23:40:48 -0400

    Revert "Revert "Changed Database from SQL Server to SQLite""
    This reverts commit 568d43927b30b6d1dfea7bd16553696b3c979c73.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Test1.Data;
+
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+	{
+	}
+
+	public DbSet<User> User { get; set; }
+}
---
+++ Models/HtmlGenerationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Test1.Models
+{
+	public class HtmlGenerationRequest
+	{
+		public string? Name { get; set; }
+		public string? Email { get; set; }
+		public string? ImageURL { get; set; }	
+	}
+}
